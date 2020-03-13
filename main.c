94168c31086e1654765d32ccb055172ab03b5069
Arseniy Kononov <akononov@l1-3-chebl.inp.nsk.su>
2020-03-13 17:10:46 +0700

    Update

--- main.c
+++ main.c
@@ -1,14 +1,30 @@
 #include <stdio.h>
 #include <CAENVMElib.h>
 
+const CVAddressModifier addr_mod = cvA32_U_DATA; // A32 non-privileged data access
+const CVDataWidth data_width = cvD32;
 
+int read(int32_t handle, uint32_t address, uint32_t *data) {
+	CVErrorCodes cverr = CAENVME_ReadCycle(handle, address, data, addr_mod, data_width);
+	switch (cverr) {
+		case cvSuccess:
+			return 0;
+		case cvBusError:
+			printf("BusError\n");
+			break;				   
+		case cvCommError:
+			printf("Communication Error !!!");
+			break;
+		default:
+			printf("Unknown Error !!!");
+			break;
+	}
+	return 1;
+}
 
 int main(int argc, char **argv) {
 	short device, link;
 	int32_t handle;
-	CVErrorCodes cverr = cvSuccess;
-
-	unsigned short addr_mod = cvA32_U_DATA; // A32 non-privileged data access
 
 	if (argc != 3) {
 		printf("Usage: test VMEDevice VMELink");
@@ -30,20 +46,8 @@ int main(int argc, char **argv) {
 	uint32_t data;
 
 	
-	cverr = CAENVME_ReadCycle(handle, base + offset, &data, addr_mod, cvD32);
-	switch (cverr) {
-		case cvSuccess:
-			printf("Value: %08X\n", data);
-			break;
-		case cvBusError:
-			printf("BusError\n");
-			break;				   
-		case cvCommError:
-			printf(" Communication Error !!!");
-			break;
-		default:
-			printf(" Unknown Error !!!");
-			break;
+	if (read(handle, base + offset, &data) == 0) {
+		printf("Value: %08X\n", data);
 	}
 
 	CAENVME_End(handle);
