2e842c8fca9148d4dc2d52573bcb32398062e3d6
Arseniy Kononov <kononovarseniy@gmail.com>
2020-05-05 16:57:56 +0700

    Read device information from vsdc3

--- main.c
+++ main.c
@@ -1,64 +1,100 @@
 #include <stdio.h>
 #include <CAENVMElib.h>
 
+#include "vsdc3.h"
+
 const CVAddressModifier addr_mod = cvA32_U_DATA; // A32 non-privileged data access
 const CVDataWidth data_width = cvD32;
 
-int read(int32_t handle, uint32_t address, uint32_t *data) {
-	CVErrorCodes cverr = CAENVME_ReadCycle(handle, address, data, addr_mod, data_width);
-	switch (cverr) {
+
+void printError(const char *msg, CVErrorCodes err) {
+	printf("%s: ", msg);
+	switch (err) {
 		case cvSuccess:
-			return 0;
+			printf("Success\n");
+			return;
 		case cvBusError:
-			printf("BusError\n");
+			printf("VME bus error\n");
 			break;				   
 		case cvCommError:
-			printf("Communication Error !!!");
+			printf("Communication error\n");
+			break;
+		case cvGenericError:
+			printf("Unspecified error\n");
+			break;
+		case cvInvalidParam:
+			printf("Invalid parameter\n");
+			break;
+		case cvTimeoutError:
+			printf("Timeout error\n");
 			break;
 		default:
-			printf("Unknown Error !!!");
+			printf("Unknown Error\n");
 			break;
 	}
-	return 1;
 }
 
-#define DEVICE_ID 0x01FFFFC0
-#define REF_H 0x01FFFFCC
-#define REF_L 0x01FFFFD0
-#define TIME_QUANT 0x01FFFFD4
+CVErrorCodes read(int32_t handle, uint32_t address, uint32_t *data) {
+	return CAENVME_ReadCycle(handle, address, data, addr_mod, data_width);
+}
+	
+
+struct device_info {
+	int swid;
+	int hwid;
+	int devid;
+};
+
+void print_device_info(struct device_info *info) {
+	printf("Device: 0x%04X\n", info->devid);
+	printf("Hardware: 0x%02X\n", info->hwid);
+	printf("Software: 0x%02X\n", info->swid);
+}
+
+CVErrorCodes get_device_info(int32_t handle, uint32_t base, struct device_info *info) {
+	uint32_t device_id;
+	CVErrorCodes cverr = read(handle, base + DEV_ID, &device_id);
+	if (cverr)
+		return cverr;
+	info->swid = device_id & 0xff;
+	info->hwid = (device_id >> 8) & 0xff;
+	info->devid = device_id >> 16;
+	return cvSuccess;
+}
 
 int main(int argc, char **argv) {
 	short device, link;
 	int32_t handle;
 
 	if (argc != 3) {
-		printf("Usage: test VMEDevice VMELink");
+		printf("Usage: test VMEDevice VMELink\n");
 		return 1;
 	}
 	device = atoi(argv[1]);
 	link = atoi(argv[2]);
 	
-	if (CAENVME_Init(cvV2718, link, device, &handle)) {
-		printf("Error opening device\n");
+	CVErrorCodes cverr = CAENVME_Init(cvV2718, link, device, &handle);
+	if (cverr) {
+		printError("Error opening device", cverr);
 		return 1;
 	}
 
-	printf("Connected!!!\n");
-
+	printf("Connection successful\n");
 	uint32_t base = 0xc0000000; // 0x44
+	printf("Base address: 0x%08X\n", base);
+	printf("\n");
 
-	uint32_t data;
+	struct device_info info;
+	cverr = get_device_info(handle, base, &info);
+	if (cverr) {
+		printError("get_device_info", cverr);
+		return 1;
+	}
+	print_device_info(&info);
+
 	float quant;
-
-	
-	if (read(handle, base + DEVICE_ID, &data) == 0)
-		printf("DEV_ID: %08X\n", data);
-	if (read(handle, base + REF_H, &data) == 0)
-		printf("REF_H: %08X\n", data);
-	if (read(handle, base + REF_L, &data) == 0)
-		printf("REF_L: %08X\n", data);
 	if (read(handle, base + TIME_QUANT, (uint32_t *)&quant) == 0)
-		printf("TIME_QUANT: %f\n", quant);
+		printf("TIME_QUANT: %.9f\n", quant);
 	
 
 	CAENVME_End(handle);
